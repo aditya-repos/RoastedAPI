e920477a5c95a12cd53b7b28be3262253bac14d1
Aditya Gaurav <agaurav@mobileiron.com>
2021-01-21 01:08:23 +0530

    Array problems

---
+++ src/main/java/com/api/client/TestClient.java
@@ -0,0 +1,12 @@
+package com.api.client;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.entity.mime.content.ContentBody;
+
+public class TestClient {
+
+    public static void main(String[] args) {
+        HttpClient client;
+        ContentBody body;
+    }
+}
